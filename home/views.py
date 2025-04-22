2441c95ecb1d906fb92b0030a3a77243ca7c7582
Arina-Goldobina <riscka15@gmail.com>
2025-04-22 06:11:26 +0000

    Tue, Apr 22, 2025, 1:11 PM +07:00

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-from django.http import
+from django.http import HttpResponse
+
 # Create your views here.
+def credits(request):
+    content = "Nicky\nArina"
+    return HttpResponse(content, content_type="text/plain")
